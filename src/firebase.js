dbb6bc0792946819ac5b423e31f38e58b23394bb
davidjian0905 <davidjian0905@gmail.com>
2024-10-16 15:28:16 +0800

    fix build for deployment

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,22 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDbSH12i-KpDQqYpqJXniiAD3fyU7drJAk",
+  authDomain: "treetherium.firebaseapp.com",
+  projectId: "treetherium",
+  storageBucket: "treetherium.appspot.com",
+  messagingSenderId: "447506327762",
+  appId: "1:447506327762:web:8fae710b5a698cc18a250b",
+};
 
 // Initialize Firebase only if it hasn't been initialized yet
 let app;
 if (!getApps().length) {
-    app = initializeApp(firebaseConfig);
+  app = initializeApp(firebaseConfig);
 } else {
-    app = getApps()[0]; // Use the initialized app
+  app = getApps()[0]; // Use the initialized app
 }
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDbSH12i-KpDQqYpqJXniiAD3fyU7drJAk",
-    authDomain: "treetherium.firebaseapp.com",
-    projectId: "treetherium",
-    storageBucket: "treetherium.appspot.com",
-    messagingSenderId: "447506327762",
-    appId: "1:447506327762:web:8fae710b5a698cc18a250b"
-  };
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
