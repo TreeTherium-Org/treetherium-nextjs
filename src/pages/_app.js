fc8afbcea9b20fa77b19ee1bc0a5ce53cd36b1a5
davidjian0905 <davidjian0905@gmail.com>
2024-11-05 05:06:03 +0800

    added Roboto font library using next/font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,33 @@
 // src/pages/_app.js or pages/_app.js
 
 // Import CSS files
-import '../../public/assets/css/responsive.css';
-import '../../public/assets/css/style.css';
-import '../../public/assets/css/components/_buttons.css';
-import '../../public/assets/demo/css/demo.css';
-import '../../public/assets/css/vendor.css';
-import { SessionProvider } from 'next-auth/react';
-
+import "../../public/assets/css/responsive.css";
+import "../../public/assets/css/style.css";
+import "../../public/assets/css/components/_buttons.css";
+import "../../public/assets/demo/css/demo.css";
+import "../../public/assets/css/vendor.css";
+import { SessionProvider } from "next-auth/react";
+import { Roboto } from "@next/font/google";
 
 // Import Font files (if needed for specific font settings)
 // You don't need to explicitly import fonts in most cases as they're linked in the CSS.
 // However, if you have custom fonts in your Sass, you may want to import font settings in the Sass file.
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+});
+
 // Function to initialize the app
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
-
-
 }
 
 export default MyApp;
