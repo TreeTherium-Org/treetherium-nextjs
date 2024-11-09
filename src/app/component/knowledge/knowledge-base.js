723f2810f76d222f87216b50d5813deae8a0e3f9
davidjian0905 <davidjian0905@gmail.com>
2024-11-09 09:33:51 +0800

    refactored code in knowledge-base.js

--- src/app/component/knowledge/knowledge-base.js
+++ src/app/component/knowledge/knowledge-base.js
@@ -1,88 +1,105 @@
 import Section from "../../component/layouts/Section.js";
 import Link from "next/link";
 
+const knowledgeBaseSections = [
+  {
+    name: "TreeTherium",
+    pages: [
+      {
+        name: "What is TreeTherium",
+        link: "/knowledge-base/what-is-treetherium",
+      },
+      {
+        name: "Why Solana",
+        link: "/knowledge-base/why-solana",
+      },
+      {
+        name: "Investors",
+        link: "/knowledge-base/investors",
+      },
+    ],
+  },
+  {
+    name: "Blockchain & Crypto",
+    pages: [
+      {
+        name: "NFT's & Tree Planting",
+        link: "/knowledge-base/nfts-tree-planting",
+      },
+      {
+        name: "What Is Crypto",
+        link: "/knowledge-base/what-is-crypto",
+      },
+      {
+        name: "What Is A Crypto Wallet",
+        link: "/knowledge-base/what-is-a-crypto-wallet",
+      },
+      {
+        name: "What Is Blockchain",
+        link: "/knowledge-base/what-is-blockchain",
+      },
+      {
+        name: "Create A Phantom Wallet",
+        link: "/knowledge-base/create-a-phantom-wallet",
+      },
+      {
+        name: "Create A Jupiter Account",
+        link: "/knowledge-base/create-a-jupiter-account",
+      },
+      {
+        name: "Create A Raydium Account",
+        link: "/knowledge-base/create-a-raydium-account",
+      },
+    ],
+  },
+  {
+    name: "Reforestation",
+    pages: [
+      {
+        name: "Landowners",
+        link: "/knowledge-base/landowners",
+      },
+    ],
+  },
+  {
+    name: "Tree Planting",
+    pages: [
+      {
+        name: "Tree Planting General",
+        link: "/knowledge-base/tree-planting-general",
+      },
+    ],
+  },
+];
+
 const KnowledgeBase = () => {
   return (
-    <Section
-      allNotification={false}
-      searchPopup={true}
-      title={"Knowledge Base"}
-    >
+    <Section allNotification={false} searchPopup={true} title="Knowledge Base">
       <div className="pd-top-36">
         <div className="container">
           <h3 className="form-title">&quot;knowledge empowers people&quot;</h3>
         </div>
         <div style={styles.card}>
-          <h3 style={styles.title}>TreeTherium</h3>
-          <div style={styles.divider} />
-          <h3 style={styles.page}>
-            <Link href="/what-is-treetherium">
-              <span className="underline-text">What is TreeTherium</span>
-            </Link>
-          </h3>
-          <h3 style={styles.page}>
-            <Link href="/why-solana">
-              <span className="underline-text">Why Solana</span>
-            </Link>
-          </h3>
-          <h3 style={styles.paragraph}>
-            <Link href="/investors">
-              <span className="underline-text">Investors</span>
-            </Link>
-          </h3>
-          <h3 style={styles.title}>Blockchain & Crypto</h3>
-          <div style={styles.divider} />
-          <h3 style={styles.page}>
-            <Link href="/nfts-tree-planting">
-              <span className="underline-text">NFT&apos;s & Tree Planting</span>
-            </Link>
-          </h3>
-          <h3 style={styles.page}>
-            <Link href="/what-is-crypto">
-              <span className="underline-text">What Is Crypto</span>
-            </Link>
-          </h3>
-          <h3 style={styles.page}>
-            <Link href="/what-is-a-crypto-wallet">
-              <span className="underline-text">What Is A Crypto Wallet</span>
-            </Link>
-          </h3>
-          <h3 style={styles.page}>
-            <Link href="/what-is-blockchain">
-              <span className="underline-text">What Is Blockchain</span>
-            </Link>
-          </h3>
-          <h3 style={styles.page}>
-            <Link href="/create-a-phantom-wallet">
-              <span className="underline-text">Create A Phantom Wallet</span>
-            </Link>
-          </h3>
-          <h3 style={styles.page}>
-            <Link href="/create-a-jupiter-account">
-              <span className="underline-text">Create A Jupiter Account</span>
-            </Link>
-          </h3>
-          <h3 style={styles.paragraph}>
-            <Link href="/create-a-raydium-account">
-              <span className="underline-text">Create A Raydium Account</span>
-            </Link>
-          </h3>
-
-          <h3 style={styles.title}>Reforestation</h3>
-          <div style={styles.divider} />
-          <h3 style={styles.paragraph}>
-            <Link href="/landowners">
-              <span className="underline-text">Landowners</span>
-            </Link>
-          </h3>
-
-          <h3 style={styles.title}>Tree Planting</h3>
-          <div style={styles.divider} />
-          <h3 style={styles.page}>
-            <Link href="/tree-planting-general">
-              <span className="underline-text">Tree Planting General</span>
-            </Link>
-          </h3>
+          {knowledgeBaseSections.map((section, index) => (
+            <div key={index}>
+              <h3 style={styles.title}>{section.name}</h3>
+              <div style={styles.divider} />
+              {section.pages.map((page, pageIndex) => (
+                <h3
+                  key={pageIndex}
+                  style={{
+                    ...styles.page,
+                    marginBottom:
+                      pageIndex === section.pages.length - 1 ? "28px" : "0px",
+                  }}
+                >
+                  <Link href={page.link}>
+                    <span className="underline-text">{page.name}</span>
+                  </Link>
+                </h3>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </Section>
@@ -92,11 +109,6 @@ const KnowledgeBase = () => {
 export default KnowledgeBase;
 
 const styles = {
-  body: {
-    activeModal: {
-      overflowY: "hidden",
-    },
-  },
   card: {
     backgroundColor: "#fff" /* White background for the card */,
     boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)" /* Soft shadow */,
@@ -119,42 +131,12 @@ const styles = {
     borderRadius: "50px",
     backgroundColor: "#e7e1ee",
   },
-  paragraph: {
-    fontSize: "1.125em",
-    fontWeight: 400,
-    color: "#4F3738",
-    padding: "0 0 39px 55px",
-    marginBottom: "0px",
-  },
   page: {
     fontSize: "1.125em",
     fontWeight: 400,
     color: "#4F3738",
     padding: "0 0 11px 55px",
     marginBottom: "0px",
-  },
-  btn: {
-    display: "flex",
-    justifyContent: "space-between",
-    marginTop: "20px",
-  },
-  modal: {
-    width: "100vw",
-    height: "100vh",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    position: "fixed",
-    zIndex: 999,
-  },
-  overlay: {
-    background: "rgba(49, 49, 49, 0.8)",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
   },
   // Adding a responsive layout to make sure the text and dividers adjust well to screen size
   "@media (min-width: 600px)": {
@@ -170,9 +152,6 @@ const styles = {
       fontSize: "1.5rem",
     },
     page: {
-      fontSize: "1.125rem",
-    },
-    paragraph: {
       fontSize: "1.125rem",
     },
   },
